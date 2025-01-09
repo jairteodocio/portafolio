6ad19e0609fc008d2d3afe76fa943a28cd3a4874
jairteodocio <jairerickiker@gmail.com>
2025-01-08 22:34:46 -0600

    Actualizacion

---
+++ lib/experiencia/puntodeventa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Puntodeventa extends StatefulWidget {
+  const Puntodeventa({super.key});
+
+  @override
+  State<Puntodeventa> createState() => _PuntodeventaState();
+}
+
+class _PuntodeventaState extends State<Puntodeventa> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
